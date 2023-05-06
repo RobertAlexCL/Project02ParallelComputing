a7996659094ec8455aa43b31b2346e8ebe6f365a
Arem58 <arem.9958@gmail.com>
2023-05-06 01:44:36 -0600

    Avances con la libreria openssl

--- bruteforceNaive.c
+++ bruteforceNaive.c
@@ -10,99 +10,102 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
+#include <unistd.h>
 #include <openssl/des.h>
 
-void decrypt(DES_cblock key, char *ciph, int len) {
-  DES_key_schedule ks;
-  DES_set_key_unchecked(&key, &ks);
-  DES_ncbc_encrypt((unsigned char *)ciph, (unsigned char *)ciph, len, &ks, &key, DES_DECRYPT);
+void encrypt(const_DES_cblock *key, const unsigned char *plaintext, int plaintext_len, unsigned char *ciphertext) {
+    DES_key_schedule schedule;
+    DES_set_key_unchecked(key, &schedule);
+    DES_ecb_encrypt((const_DES_cblock *)plaintext, (DES_cblock *)ciphertext, &schedule, DES_ENCRYPT);
 }
 
-void encrypt(DES_cblock key, char *ciph, int len) {
-  DES_key_schedule ks;
-  DES_set_key_unchecked(&key, &ks);
-  DES_ncbc_encrypt((unsigned char *)ciph, (unsigned char *)ciph, len, &ks, &key, DES_ENCRYPT);
+void decrypt(const_DES_cblock *key, const unsigned char *ciphertext, int ciphertext_len, unsigned char *plaintext) {
+    DES_key_schedule schedule;
+    DES_set_key_unchecked(key, &schedule);
+    DES_ecb_encrypt((const_DES_cblock *)ciphertext, (DES_cblock *)plaintext, &schedule, DES_DECRYPT);
 }
 
-int tryKey(DES_cblock key, char *ciph, int len) {
-  char search[] = "es una prueba de";
-  char temp[len + 1];
-  memcpy(temp, ciph, len);
-  temp[len] = 0;
-  decrypt(key, temp, len);
-  return strstr(temp, search) != NULL;
+int tryKey(const_DES_cblock *key, const unsigned char *ciphertext, int ciphertext_len) {
+    char search[] = "es una prueba de";
+    unsigned char temp[ciphertext_len + 1];
+    memcpy(temp, ciphertext, ciphertext_len);
+    temp[ciphertext_len] = 0;
+    decrypt(key, temp, ciphertext_len, temp);
+    return strstr((char *)temp, search) != NULL;
 }
 
-char eltexto[] = "Esta es una prueba de proyecto 2";
-DES_cblock the_key = {0x01, 0x23, 0x45, 0x67, 0x89, 0xab, 0xcd, 0xef};
+unsigned char eltexto[] = "Esta es una prueba de proyecto 2";
+long the_key = 123456L;
 
 int main(int argc, char *argv[]) {
-  int N, id;
-  long upper = (1L << 56);
-  long mylower, myupper;
-  MPI_Status st;
-  MPI_Request req;
+    int N, id;
+    long upper = (1L << 56); 
+    long mylower, myupper;
+    MPI_Status st;
+    MPI_Request req;
 
-  int ciphlen = strlen(eltexto);
-  MPI_Comm comm = MPI_COMM_WORLD;
+    int ciphlen = strlen((char *)eltexto);
+    MPI_Comm comm = MPI_COMM_WORLD;
 
-  char cipher[ciphlen + 1];
-  memcpy(cipher, eltexto, ciphlen);
-  cipher[ciphlen] = 0;
-  encrypt(the_key, cipher, ciphlen);
+    // Cifrar el texto
+    unsigned char ciphertext[ciphlen + 1];
+    memcpy(ciphertext, eltexto, ciphlen);
+    ciphertext[ciphlen] = 0;
+    DES_key_schedule key;
+    DES_set_key_unchecked((DES_cblock *)&the_key, &key);
+    encrypt(&key, ciphertext, ciphlen, ciphertext);
 
-  MPI_Init(NULL, NULL);
-  MPI_Comm_size(comm, &N);
-  MPI_Comm_rank(comm, &id);
+    // Inicializar MPI
+    MPI_Init(NULL, NULL);
+    MPI_Comm_size(comm, &N);
+    MPI_Comm_rank(comm, &id);
 
-  long found = 0L;
-  int ready = 0;
+    long found = 0L;
+    int ready = 0;
 
-  long range_per_node = upper / N;
-  mylower = range_per_node * id;
-  myupper = range_per_node * (id + 1) - 1;
-  if (id == N - 1) {
-    myupper = upper;
-  }
-  printf("Process %d lower %ld upper %ld\n", id, mylower, myupper);
+    // Distribuir trabajo de forma naive
+    long range_per_node = upper / N;
+    mylower = range_per_node * id;
+    myupper = range_per_node * (id + 1) - 1;
+    if (id == N - 1) {
+        // Compensar residuo
+        myupper = upper;
+    }
+    printf("Process %d lower %ld upper %ld\n", id, mylower, myupper);
 
-  MPI_Irecv(&found, 1, MPI_LONG, MPI_ANY_SOURCE, MPI_ANY_TAG, comm, &req);
+    // Non blocking receive, revisar en el for si alguien ya encontró
+    MPI_Irecv(&found, 1, MPI_LONG, MPI_ANY_SOURCE, MPI_ANY_TAG, comm, &req);
 
-  for (long i = mylower; i < myupper; ++i) {
-    DES_cblock key_candidate;
-    for (int j = 0; j < 8; ++j) {
-      key_candidate[j] = (i >> (56 - 8 * (j + 1))) & 0xff;
+    for (long i = mylower; i < myupper; ++i) {
+        MPI_Test(&req, &ready, MPI_STATUS_IGNORE);
+        if (ready)
+            break; // Ya encontraron, salir
+
+        DES_key_schedule temp_key; // Cambiado a DES_key_schedule
+        DES_set_key_unchecked((DES_cblock *)&i, &temp_key); // Cambiado a &temp_key
+        printf("Process %d trying key %ld\n", id, i); // Mensaje de depuración
+
+        if (tryKey(&temp_key, ciphertext, ciphlen)) {
+            found = i;
+            printf("Process %d found the key\n", id);
+            for (int node = 0; node < N; node++) {
+                MPI_Send(&found, 1, MPI_LONG, node, 0, comm); // Avisar a otros
+            }
+            break;
+        }
     }
 
-    MPI_Test(&req, &ready, MPI_STATUS_IGNORE);
-    if (ready)
-      break;
-
-    if (tryKey(key_candidate, cipher, ciphlen)) {
-      found = i;
-      printf("Process %d found the key\n", id);
-      for (int node = 0; node < N; node++) {
-        MPI_Send(&found, 1, MPI_LONG, node, 0, comm); //avisar a otros
-      }
-      break;
+    // Wait y luego imprimir el texto
+    if (id == 0) {
+        MPI_Wait(&req, &st);
+        DES_cblock found_key;
+        DES_set_key_unchecked((DES_cblock *)&the_key, &key);
+        decrypt(&found_key, ciphertext, ciphlen, ciphertext);
+        printf("Key = %li\n\n", found);
+        printf("%s\n", ciphertext);
     }
-  }
+    printf("Process %d exiting\n", id);
 
-  if (id == 0) {
-    MPI_Wait(&req, &st);
-    for (int j = 0; j < 8; ++j) {
-      the_key[j] = (found >> (56 - 8 * (j + 1))) & 0xff;
-    }
-    decrypt(the_key, cipher, ciphlen);
-    printf("Key = %li\n\n", found);
-    printf("Key bytes: ");
-    for (int j = 0; j < 8; ++j) {
-      printf("%02x ", the_key[j]);
-    }
-    printf("\n\n");
-    printf("%s\n", cipher);
-  }
-  printf("Process %d exiting\n", id);
-
-  MPI_Finalize();
+    // Finalizar entorno MPI
+    MPI_Finalize();
 }
